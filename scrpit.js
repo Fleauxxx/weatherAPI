77e58f3ae74cdc1bc0c0156d24d12c13cc460e8a
Floyd Hudson <hudsonfloyd@yahoo.com>
2022-11-21 18:33:01 -0600

    added icon url and took the for loop off of the data everything is working

--- scrpit.js
+++ scrpit.js
@@ -1,12 +1,13 @@
 var city = document.getElementById("city");
 var searchBtn = document.getElementById("searchBtn");
 var apiKey = "1b531a9977e00deaae0dca3d2fcfaf62";
-var today = document.getElementById("cityContainer");
-var one = document.getElementById("one");
+var contain = document.getElementById("cityContainer");
+var day1 = document.getElementById("one");
 var day2 = document.getElementById("two");
 var day3 = document.getElementById("three");
 var day4 = document.getElementById("four");
 var day5 = document.getElementById("five");
+
 
 function getApi() {
   var requestForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city.value}&appid=${apiKey}&units=imperial`;
@@ -20,35 +21,100 @@ function getApi() {
       console.log(data.list[1].dt);
       console.log(data.list[1].weather[0].icon);
 
-      for (var i = 0; i < data.list.length; i++) {
-        var listDate = document.createElement("p");
-        var listIcon = document.createElement("p");
+     
+        var iconCode = data.list[0].weather[0].icon;
+        var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+        var listIcon = document.createElement("img");
         var listTemp = document.createElement("p");
         var listHumid = document.createElement("p");
         var listWind = document.createElement("p");
+        
+        var iconCode2 = data.list[1].weather[0].icon;
+        var iconUrl2 = "http://openweathermap.org/img/wn/" + iconCode2 + "@2x.png";
+        var listIcon2 = document.createElement("img");
+        var listTemp2= document.createElement("p");
+        var listHumid2 = document.createElement("p");
+        var listWind2 = document.createElement("p");
 
-        listDate.textContent = " Date " + data.list[0].dt;
-        console.log(listDate);
-        listIcon.textContent = data.list[0].weather[0].icon;
+        var iconCode3 = data.list[2].weather[0].icon;
+        var iconUrl3 = "http://openweathermap.org/img/wn/" + iconCode3 + "@2x.png";
+        var listIcon3 = document.createElement("img");
+        var listTemp3 = document.createElement("p");
+        var listHumid3 = document.createElement("p");
+        var listWind3 = document.createElement("p");
+        
+        var iconCode4 = data.list[3].weather[0].icon;
+        var iconUrl4 = "http://openweathermap.org/img/wn/" + iconCode4 + "@2x.png";
+        var listIcon4 = document.createElement("img");
+        var listTemp4 = document.createElement("p");
+        var listHumid4 = document.createElement("p");
+        var listWind4 = document.createElement("p");
+        
+        var iconCode5 = data.list[4].weather[0].icon;
+        var iconUrl5 = "http://openweathermap.org/img/wn/" + iconCode5 + "@2x.png";
+        var listIcon5 = document.createElement("img");
+        var listTemp5 = document.createElement("p");
+        var listHumid5 = document.createElement("p");
+        var listWind5 = document.createElement("p");
+
+      
+        listIcon.setAttribute("src" , iconUrl);
         listTemp.textContent = " Temp " + data.list[0].main.temp + " deg ";
         listHumid.textContent = " Humidity " + data.list[0].main.humidity + " % ";
         listWind.textContent = " Wind " + data.list[0].wind.speed + " mph ";
-
-        day2.appendChild(listIcon);
-        day2.appendChild(listTemp);
-        day2.appendChild(listHumid);
-        day2.appendChild(listWind);
         
-        listIcon.textContent = data.list[0].weather[0].icon;
-        listTemp.textContent = " Temp " + data.list[1].main.temp + " deg ";
-        listHumid.textContent = " Humidity " + data.list[1].main.humidity + " % ";
-        listWind.textContent = " Wind " + data.list[1].wind.speed + " mph ";
+        day1.appendChild(listIcon);
+        day1.appendChild(listTemp);
+        day1.appendChild(listHumid);
+        day1.appendChild(listWind);
+
+        listIcon2.setAttribute("src" , iconUrl2);
+        listIcon2.textContent = data.list[1].weather[0].icon;
+        listTemp2.textContent = " Temp " + data.list[1].main.temp + " deg ";
+        listHumid2.textContent = " Humidity " + data.list[1].main.humidity + " % ";
+        listWind2.textContent = " Wind " + data.list[1].wind.speed + " mph ";
       
-        day3.appendChild(listIcon);
-        day3.appendChild(listTemp);
-        day3.appendChild(listHumid);
-        day3.appendChild(listWind);
-      }
+        day2.appendChild(listIcon2);
+        day2.appendChild(listTemp2);
+        day2.appendChild(listHumid2);
+        day2.appendChild(listWind2);
+
+        listIcon3.setAttribute("src" , iconUrl3);
+        listIcon3.textContent = data.list[2].weather[0].icon;
+        listTemp3.textContent = " Temp " + data.list[2].main.temp + " deg ";
+        listHumid3.textContent = " Humidity " + data.list[2].main.humidity + " % ";
+        listWind3.textContent = " Wind " + data.list[2].wind.speed + " mph ";
+
+        
+        day3.appendChild(listIcon3);
+        day3.appendChild(listTemp3);
+        day3.appendChild(listHumid3);
+        day3.appendChild(listWind3);
+
+        listIcon4.setAttribute("src" , iconUrl4);
+        listIcon4.textContent = data.list[3].weather[0].icon;
+        listTemp4.textContent = " Temp " + data.list[3].main.temp + " deg ";
+        listHumid4.textContent = " Humidity " + data.list[3].main.humidity + " % ";
+        listWind4.textContent = " Wind " + data.list[3].wind.speed + " mph ";
+
+        day4.appendChild(listIcon4);
+        day4.appendChild(listTemp4);
+        day4.appendChild(listHumid4);
+        day4.appendChild(listWind4);
+
+        listIcon5.setAttribute("src" , iconUrl5);
+        listIcon5.textContent = data.list[4].weather[0].icon;
+        listTemp5.textContent = " Temp " + data.list[4].main.temp + " deg ";
+        listHumid5.textContent = " Humidity " + data.list[4].main.humidity + " % ";
+        listWind5.textContent = " Wind " + data.list[4].wind.speed + " mph ";
+
+        day5.appendChild(listIcon5);
+        day5.appendChild(listTemp5);
+        day5.appendChild(listHumid5);
+        day5.appendChild(listWind5);
+
+
+      // }
     });
 }
 
